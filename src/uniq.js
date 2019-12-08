afca2279973ac5f255ce5d1a6339d36dbf63b4bd
Jang Rush <weakish@gmail.com>
2019-12-08 14:55:31 +0800

    :art: add semicolons

--- src/uniq.js
+++ src/uniq.js
@@ -1,10 +1,10 @@
-"use strict"
-const getStdin = require("get-stdin")
+"use strict";
+const getStdin = require("get-stdin");
 
 /** @type {function(string): string} */
-const uniq = lines => [...new Set(lines.split("\n"))].join("\n")
+const uniq = lines => [...new Set(lines.split("\n"))].join("\n");
 
 /** @type {function(): Promise<void>} */
-const main = (async () => console.log(uniq(await getStdin())))
+const main = (async () => console.log(uniq(await getStdin())));
 
-main()
+main();
